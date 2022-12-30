fdb7a3086094dfbe4dd95665b5e53a99d648e764
Kaspar Triebstok <kaspartr@gmail.com>
2022-12-30 17:28:15 +0800

    public vars

---
+++ src/assets/constants/index.js
@@ -0,0 +1,8 @@
+const VAR = {
+    ENV: "staging",
+    STAGE_ADMIN_BASE_URL: "https://stage.admin.redcurry.co/",
+    ADMIN_BASE_URL: "https://admin.redcurry.co/",
+    MAGIC_PUBLIC_KEY:"pk_live_DB4074FE2B7C10D9"
+}
+
+export {VAR}
