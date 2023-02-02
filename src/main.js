5ef1dd1f90ebe1f2b70ac015c3761a05d572820d
Kaspar Triebstok <kaspartr@gmail.com>
2023-02-02 11:40:28 +0800

    Removed the now ignored constants file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, BIcon, BIconArrowUp, BIconArrowDown, BIconXCircle } from 'bootstrap-vue'
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import {VAR} from './assets/constants/index'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
@@ -10,6 +13,22 @@ Vue.component('BIcon', BIcon)
 Vue.component('BIconArrowUp', BIconArrowUp)
 Vue.component('BIconArrowDown', BIconArrowDown)
 Vue.component('BIconXCircle', BIconXCircle)
+
+Sentry.init({
+	Vue,
+	dsn: VAR.SENTRY_DNS,
+	integrations: [
+	  new BrowserTracing({
+		routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+		tracePropagationTargets: ["localhost", VAR.APP_DOMAIN, /^\//],
+	  }),
+	],
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+  });
+  
 
 new Vue({
 	router,
